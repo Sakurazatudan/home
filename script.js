bd38ea0b8f76b3f437f4f8f60af3e08826e70e50
さくら雑談王国 <Jiken7777@gmail.com>
2025-02-02 10:27:30 +0900

    Update script.js

--- script.js
+++ script.js
@@ -8,5 +8,5 @@ window.onload = function() {
             welcomeScreen.style.display = "none"; // 完全に非表示
             mainContent.classList.remove("hidden"); // メインコンテンツ表示
         }, 800); // フェードアウト時間
-    }, 3400); // ウェルカムスクリーンの表示時間
+    }, 3000); // ウェルカムスクリーンの表示時間
 };
